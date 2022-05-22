2fa5a06264da498758c84c5000f554e8a0c9fe8c
Tanmay Gangurde <tanmay.gangurde@gmail.com>
2022-05-23 00:23:43 +0530

    Updated DES

--- ICS/des.py
+++ ICS/des.py
@@ -2,7 +2,11 @@ from Crypto.Cipher import DES
 
 def pad(text):
     n = len(text) % 8
-    return text + (b' ' * n)
+    print(len(text))
+    print(n)
+    print(text + (b' ' * (8 - n)))
+    print()
+    return text + (b' ' * (8-n))
 
 
 key = b'hello123'
@@ -13,5 +17,7 @@ des = DES.new(key, DES.MODE_ECB)
 padded_text = pad(text1)
 encrypted_text = des.encrypt(padded_text)
 
+print()
 print(encrypted_text)
 print(des.decrypt(encrypted_text))
+
